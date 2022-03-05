abb095e3237ff50239fab8db4e2b2c5ef6c3d0ff
fzarco <filipezarcocoutinho@gmail.com>
2022-03-05 22:43:05 +0000

    changed ligavel from motor to carro

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -1,10 +1,24 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoDesligadoException
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoLigadoException
+import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import java.time.format.DateTimeFormatter
 
-data class Carro(override val identificador : String, var motor : Motor) : Veiculo(identificador), Movimentavel
+data class Carro(override val identificador : String, var motor : Motor) : Veiculo(identificador), Movimentavel,Ligavel
 {
+    override fun ligar() {
+        if (motor.ligado)
+            throw VeiculoLigadoException("Veiculo já se encontra ligado")
+        motor.ligado = true
+    }
+    override fun desligar() {
+        if (!motor.ligado)
+            throw VeiculoDesligadoException("Veiculo já se encontra desligado")
+        motor.ligado = false
+    }
+
     override fun requerCarta(): Boolean {
         return true
     }
@@ -12,6 +26,5 @@ data class Carro(override val identificador : String, var motor : Motor) : Veicu
     override fun toString(): String {
         return "Carro | $identificador | $dataDeAquisicao | $posicao"
     }
-
 
 }
--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -2,18 +2,11 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 
-data class Motor (val cavalos : Int,val cilindrada : Int) : Ligavel {
+data class Motor (val cavalos : Int,val cilindrada : Int){
     var ligado : Boolean = false
 
-    override fun ligar() {
-        this.ligado = true
-    }
-    override fun desligar() {
-        this.ligado = false
-    }
     override fun toString(): String {
         return "Motor | $cavalos | $cilindrada"
     }
-
 
 }
